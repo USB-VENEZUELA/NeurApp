1ce6c482d843ca89408d6dc32d418513e3d8e5ae
Cesar <tratohecho3@gmail.com>
2017-10-23 09:24:14 -0400

    contacto

--- client/js/google_map_contacto.js
+++ client/js/google_map_contacto.js
@@ -1,7 +1,7 @@
 function initMap() {
-	var uluru = {lat: -25.363, lng: 131.044};
+	var uluru = {lat: 20.707938, lng: -100.446606};
 	var map = new google.maps.Map(document.getElementById('map'), {
-	zoom: 4,
+	zoom: 14,
 	center: uluru
 	});
 	var marker = new google.maps.Marker({
